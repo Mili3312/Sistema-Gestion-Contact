3f280f5c5831b0fd8bc52f98845aa8703ef4dd53
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-11 01:29:12 -0300

    función para obtener un contacto por su ID

--- back-end/src/controllers/contactController.js
+++ back-end/src/controllers/contactController.js
@@ -20,8 +20,23 @@ const  createContact = async (req, res) => {
         res.status(400).json({error: err.message});
      }
 }
+const getContactById = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const contact = await Contact.findById(id)
+
+        if (!contact) {
+            return res.status(404).json({message:"contact not found"})
+        }
+        res.status(200).json(contact)
+    } catch (err) {
+        res.status(500).json({message: "Error fetching contact", error: err.message})
+    }
+}
 
 module.exports={
     getAllContacts,
-    createContact
+    createContact,
+    getContactById
 }
