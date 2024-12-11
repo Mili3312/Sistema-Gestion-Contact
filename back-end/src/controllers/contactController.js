ef660f3f3e0fb7aca78d82333841a0a4b27996cc
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-11 02:25:39 -0300

    función para eliminar contacto

--- back-end/src/controllers/contactController.js
+++ back-end/src/controllers/contactController.js
@@ -53,6 +53,19 @@ const updateContact = async (req,res) => {
         res.status(500).json({message:"Error update contact", error:err.message})
     }
 }
+const deleteContact = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const contact = await Contact.findByIdAndDelete(id);
+
+        if (!contact) {
+            return res.status(404).json({message:"Contact not found"})
+        }
+        res.status(200).json({message:"Contact delete successfully"})
+    } catch (err) {
+        res.status(500).json({message:"Error delete contact", error:err.message})
+    }
+}
 module.exports={
     getAllContacts,
     createContact,
