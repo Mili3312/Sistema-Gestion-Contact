9e039d742d4300bea23b73d0f8eaa176e97e6225
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-11 10:59:43 -0300

    rutas put y delete

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 require('dotenv').config()
 const connectDB=require('./src/config/db')
-
+const contactRoutes = require('./src/routes/contactRoutes')
 
 
 connectDB();
 
 
 const app=express();
-app.use(express.json())
+app.use(express.json());
 
+app.use('/contacts', contactRoutes);
 
 const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, ()=>{
--- back-end/src/routes/contactRoutes.js
+++ back-end/src/routes/contactRoutes.js
@@ -4,4 +4,8 @@ const contactControler= require('../controllers/contactController')
 
 router.get('/', contactControler.getAllContacts);
 router.post('/', contactControler.createContact);
-router.get('/:id', contactControler.getContactById)
+router.get('/:id', contactControler.getContactById);
+router.put('/:id', contactControler.updateContact);
+router.delete('/:id', contactControler.deleteContact)
+
+module.exports= router
