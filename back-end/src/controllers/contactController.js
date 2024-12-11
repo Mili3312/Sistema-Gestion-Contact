35fd5dfa411ddac566c9db2c03584b762775762b
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-11 00:45:08 -0300

    función para crear nuevo contacto

--- back-end/src/controllers/contactController.js
+++ back-end/src/controllers/contactController.js
@@ -1,12 +1,27 @@
-const contact = require('../models/contactModel')
+const Contact = require('../models/contactModel')
 
 
-const  gateAllContacts = async (req, res)=>{
+const  getAllContacts = async (req, res)=>{
     try {
-        const contacts = await contact.find();
+        const contacts = await Contact.find();
         res.status(200).json(contacts);
     } catch (err) {
         res.status(500).json({error: err.message})
         
     }
+}
+const  createContact = async (req, res) => {
+     try {
+        const {name, email, phone, addres, birthDate} = req.body
+        const newContact = Contact({name, email, phone, addres, birthDate})
+        await newContact.save()
+        res.status(201).json(newContact)
+     } catch (err) {
+        res.status(400).json({error: err.message});
+     }
+}
+
+module.exports={
+    getAllContacts,
+    createContact
 }
