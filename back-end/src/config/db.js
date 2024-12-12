b2e090da4d1b84333c88c3dbb475091edd66a6dd
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-12 18:48:52 -0300

    Corrección

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
+//conexión base de datos
 const connectDB = async()=>{
     try{
-        const connect = await mongoose.connect(process.env.MONGO_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            } );
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        //si la conexión es exitosa
             console.log(`MongoDB Connected: ${connect.connection.host}`)
     }catch (err){
+        //si la conexión falla
            console.error(`Error: ${err.message}`)
            process.exit(1)
     }
--- back-end/src/models/contactModel.js
+++ back-end/src/models/contactModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+
+//esquema de contacto
 const contactSchema = new mongoose.Schema({
     name:{type:String, required: true},
     email:{type:String, required: true},
-    phone:{type:Number, require: true},
-    addres:{type:String, require: true},
-    birthDate:{type:Number, require: true},
+    phone:{type:String, required: true},
+    address:{type:String, required: true},
+    birthDate:{type:Date, required: true},
 });
 
 module.exports=mongoose.model('Contact', contactSchema);
