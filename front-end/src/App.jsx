fbb669940d9d5ee6323d736ee00f0c77a4ab4fae
Mili3312 <milagrosvillafane777@gmail.com>
2024-12-17 04:44:51 -0300

    comentarios

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import  ContactList  from "./components/ContactList";
+import ContactList from "./components/ContactList";
 import ContactForm from './components/ContactForm';
 
 const App = () => {
 
-  const [contacts, setContacts]= useState([]);
-  const [editContact, setEditContact]=useState(null)
- const [showForm, setShowForm]= useState(false)
+  const [contacts, setContacts] = useState([]);
+  const [editContact, setEditContact] = useState(null)
+  const [showForm, setShowForm] = useState(false)
+
   //CARGAR CONTACTOS AL SERVIDOR
-  const fetchContacts = async()=>{
+  const fetchContacts = async () => {
     try {
-      const response =await fetch('http://localhost:5000/contacts')
+      const response = await fetch('http://localhost:5000/contacts')
       if (!response.ok) throw new Error("Error al cargar los contactos");
-      const data =await response.json();
+      const data = await response.json();
       setContacts(data)
     } catch (error) {
       console.error('No se pudo obtener los contactos')
@@ -20,58 +21,62 @@ const App = () => {
   };
 
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchContacts()
-  },[]);
-  
-  const handleContactAdded = (newContact)=>{
-    setContacts(prevContacts =>[...prevContacts, newContact])
+  }, []);
+
+  //adición de un contacto
+  const handleContactAdded = (newContact) => {
+    setContacts(prevContacts => [...prevContacts, newContact])
   }
-  const handleContactEdited = (editedContact)=>{
-    setContacts(prevContacts=>prevContacts.map(contact=>contact._id === editedContact._id ? editedContact :contact)
-  )
-setEditContact(null)
+  //edición de contacto
+  const handleContactEdited = (editedContact) => {
+    setContacts(prevContacts => prevContacts.map(contact => contact._id === editedContact._id ? editedContact : contact)
+    )
+    setEditContact(null)
 
   };
 
+  //eliminación de contacto
   const handleContactDeleted = async (contactId) => {
     try {
-      const response = await fetch(`http://localhost:5000/contacts/${contactId}`,{
-        method:'DELETE'
+      const response = await fetch(`http://localhost:5000/contacts/${contactId}`, {
+        method: 'DELETE'
       });
       if (!response.ok) {
         throw new Error('Error al eliminar el contacto')
       }
 
-      setContacts(prevContacts => prevContacts.filter(contact=>contact._id !== contactId)) //Elimina contacto del estado
+      setContacts(prevContacts => prevContacts.filter(contact => contact._id !== contactId)) //Elimina contacto del estado
     } catch (error) {
       console.log('no se pudo eliminar el contacto')
     }
   };
 
-  const toggleForm=()=>{
-    setShowForm((prevState)=> !prevState)
+  //cambiar la visibilidad del formulario
+  const toggleForm = () => {
+    setShowForm((prevState) => !prevState)
   }
   return (
     <div className='min-h-screen bg-pink-50'>
-    <div className='container mx-auto p-4'>
-      <h1 className='text-3x1 font-bold text-center mb-6 text-teal-600'>Lista de contactos</h1>
+      <div className='container mx-auto p-4'>
+        <h1 className='text-3x1 font-bold text-center mb-6 text-teal-600'>Lista de contactos</h1>
 
-{/**mostrar/ocultar formulario */}
-      <button onClick={toggleForm} className='bg-teal-600 text-white px-4 py-2 rounded-lg mb-4'>
-        {showForm ? 'cerrar formulario' : 'Agregar nuevo contacto'}
-      </button>
+        {/**mostrar/ocultar formulario */}
+        <button onClick={toggleForm} className='bg-teal-600 text-white px-4 py-2 rounded-lg mb-4'>
+          {showForm ? 'cerrar formulario' : 'Agregar nuevo contacto'}
+        </button>
 
-{showForm && ( 
-  <ContactForm onContactAdded={handleContactAdded}
-  onContactEdited={handleContactEdited}
-  editingContact={editContact}/>
-)}
+        {showForm && (
+          <ContactForm onContactAdded={handleContactAdded}
+            onContactEdited={handleContactEdited}
+            editingContact={editContact} />
+        )}
 
-      <ContactList contacts={contacts} onEdit={setEditContact} onDelete={handleContactDeleted}/>
+        <ContactList contacts={contacts} onEdit={setEditContact} onDelete={handleContactDeleted} />
 
-    </div>
       </div>
+    </div>
   )
 }
 
--- front-end/src/components/ContactForm.jsx
+++ front-end/src/components/ContactForm.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { useState } from "react";
 
-const ContactForm = ({onContactAdded, onContactEdited, editingContact}) => {
+const ContactForm = ({ onContactAdded, onContactEdited, editingContact }) => {
   //Estado para los campos del form
-  const [name, setName]= useState('');
-  const [email, setEmail]= useState('');
-  const [phone, setPhone]= useState('');
-  const [address, setAddress]= useState('');
-  const [birthDate, setBirthDate]= useState('');
-  const [errors, setErrors]= useState({});
-  const [isLoading, setIsLoading]= useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
 
-  useEffect(()=>{
+  //carga los datos para editar
+  useEffect(() => {
     if (editingContact) {
       setName(editingContact.name);
       setEmail(editingContact.email);
@@ -24,28 +25,28 @@ const ContactForm = ({onContactAdded, onContactEdited, editingContact}) => {
 
 
   /**manejo del envio del form - validaciones de datos */
-  const handleSubmit=async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    let formErrors={};
+    let formErrors = {};
 
     if (!name) {
-      formErrors.name='Name is required';
+      formErrors.name = 'Name is required';
     }
     if (!email) {
-      formErrors.email='Email is required';
+      formErrors.email = 'Email is required';
     }
     if (!phone) {
-      formErrors.phone='Phone is required';
+      formErrors.phone = 'Phone is required';
     }
     if (!address) {
-      formErrors.address='Address is required';
+      formErrors.address = 'Address is required';
     }
     if (!birthDate) {
-      formErrors.birthDate=' BirthDate is required';
+      formErrors.birthDate = ' BirthDate is required';
     }
 
-    const emailPattern =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const phonePattern = /^\+?\d{10,15}$/;
 
     if (email && !emailPattern.test(email)) {
@@ -55,37 +56,37 @@ const ContactForm = ({onContactAdded, onContactEdited, editingContact}) => {
       formErrors.phone = 'Phone is not valid'
     }
 
-    if (Object.keys(formErrors).length>0) {
+    if (Object.keys(formErrors).length > 0) {
       setErrors(formErrors);
       return;
     }
     setIsLoading(true)
-    
-    try{
+
+    try {
       let response;
       if (editingContact) {
         //EDITAR CONTACTO
         response = await fetch(`http://localhost:5000/contacts/${editingContact._id}`, {
-          method:'PUT',
-          headers:{'Content-Type': 'application/json'},
-          body:JSON.stringify({name, email, phone, address, birthDate}),
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, email, phone, address, birthDate }),
         });
-        
-      }else{
+
+      } else {
         //CREAR NUEVO CONTACTO
         response = await fetch('http://localhost:5000/contacts', {
-          method:'POST',
-          headers:{'Content-Type': 'application/json'},
-          body:JSON.stringify({name, email, phone, address, birthDate}),
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, email, phone, address, birthDate }),
         });
       }
 
-      const data=await response.json()
+      const data = await response.json()
 
       if (response.ok) {
         if (editingContact) {
           onContactEdited(data)
-        }else{   
+        } else {
           onContactAdded(data)
         }
 
@@ -95,57 +96,57 @@ const ContactForm = ({onContactAdded, onContactEdited, editingContact}) => {
         setAddress('');
         setBirthDate('');
         setErrors({});
-      }else{
-        setErrors({global: data.message || 'an unexpected error occurred'})
+      } else {
+        setErrors({ global: data.message || 'an unexpected error occurred' })
       }
-    }catch(err){
+    } catch (err) {
       console.log('Error during submission: ', err)
-      setErrors({global: 'Please try again later'})
-    }finally{
+      setErrors({ global: 'Please try again later' })
+    } finally {
       setIsLoading(false)
     }
   }
   return (
     <div className='max-w-xl mx-auto p-4 bg-white rounded-lg shadow-md'>
-      <h2 className='text-2xl font-bold text-teal-600 mb-4'>{editingContact ? 'Editar Contacto':'Nuevo contacto'}</h2>
+      <h2 className='text-2xl font-bold text-teal-600 mb-4'>{editingContact ? 'Editar Contacto' : 'Nuevo contacto'}</h2>
 
       <form onSubmit={handleSubmit} className='space-y-4'>
         <div>
           <label htmlFor="name" className='block text-lg'>Nombre</label>
-          <input type="text" id='name' name='name' value={name} onChange={(e)=>setName(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='nombre completo' />
+          <input type="text" id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='nombre completo' />
           {errors.name && <p className='text-red-500'>{errors.name}</p>}
         </div>
 
         <div>
           <label htmlFor="email" className='block text-lg'> Correo electronico</label>
-          <input type="email" name="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='correo electrónico' />
+          <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='correo electrónico' />
           {errors.email && <p className='text-red-500'>{errors.email}</p>}
         </div>
 
         <div>
           <label htmlFor="phone" className='block text-lg'> Teléfono </label>
-          <input type="text" name="phone" id="phone" value={phone} onChange={(e)=>setPhone(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='Número de teléfono' />
+          <input type="text" name="phone" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='Número de teléfono' />
           {errors.phone && <p className='text-red-500'>{errors.phone}</p>}
         </div>
 
         <div>
           <label htmlFor="address" className='block text-lg'> Dirección </label>
-          <input type="text" name="address" id="address" value={address} onChange={(e)=>setAddress(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='Dirección' />
+          <input type="text" name="address" id="address" value={address} onChange={(e) => setAddress(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' placeholder='Dirección' />
           {errors.address && <p className='text-red-500'>{errors.address}</p>}
         </div>
 
         <div>
           <label htmlFor="birthDate" className='block text-lg'> fecha de nacimiento</label>
-          <input type="date" name="birthDate" id="birthDate" value={birthDate} onChange={(e)=>setBirthDate(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100'  />
+          <input type="date" name="birthDate" id="birthDate" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} className='w-full p-2 border rounded-lg text-gray-700 bg-teal-100' />
           {errors.birthDate && <p className='text-red-500'>{errors.birthDate}</p>}
         </div>
 
         {errors.global && <p className='text-red-500'>{errors.global}</p>}
 
         <div className='mt-4'>
-          <button className='w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400'disabled={isLoading}>
+          <button className='w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400' disabled={isLoading}>
 
-{isLoading ? 'guardando.... ' : editingContact ? 'Actualizar Contacto' : 'Agregar contacto'}
+            {isLoading ? 'guardando.... ' : editingContact ? 'Actualizar Contacto' : 'Agregar contacto'}
 
           </button>
         </div>
